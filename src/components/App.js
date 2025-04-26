4fc01ccb202cd2b42314dd80bd79f6468b618ac8
edwinndungu2 <edwin.ndungu2@student.moringaschool.com>
2025-04-26 04:23:02 +0300

    complete assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import blogData from "../data/blog";
+import About from "./About";
+import Article from "./Article";
+import Header from "./Header";
 
 console.log(blogData);
 
 function App() {
-  return (
+	return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
-    </div>
-  );
+      <Header name={blogData.name} />
+      <About />
+      <Article />
+		</div>
+	);
 }
 
 export default App;
